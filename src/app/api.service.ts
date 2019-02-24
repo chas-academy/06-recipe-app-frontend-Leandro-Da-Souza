3ea718761c455077427c2042cc0729a50d23a259
Leandro Da Souza <leandrodasouza@Leandros-MacBook-Pro.local>
2019-02-24 13:06:46 +0100

    update styles in angular.json, add hardcoded recepies in api.service for testing purposes

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  recepies = [
+    'Spicy MeatBall',
+    'Fideo con Tuco',
+    'Pompom Famous Chili',
+    'Lasanga',
+    'Cheeseburger',
+    'Eel',
+    'Human'
+  ]
+
+  constructor(private http: HttpClient) { }
+
+  getRecepies = () => {
+    return this.recepies;
+  }
+
 }
